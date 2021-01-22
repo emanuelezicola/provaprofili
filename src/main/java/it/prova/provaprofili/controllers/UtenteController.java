bc83f9789f0d2b98a466fe968de538a53b9c0b47
Emanuele Zicola <e.zicola@solvingteam.it>
2021-01-22 16:46:10 +0100

    Fix driver mysql e RequestBody mancante

--- src/main/java/it/prova/provaprofili/controllers/UtenteController.java
+++ src/main/java/it/prova/provaprofili/controllers/UtenteController.java
@@ -7,10 +7,7 @@ import it.prova.provaprofili.services.UtenteService;
 import it.prova.provaprofili.utils.dtovalidations.DtoValidations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.LinkedHashMap;
@@ -31,7 +28,7 @@ public class UtenteController {
 
 
     @PostMapping("/registrazione")
-    public ResponseEntity<Object> registrazione(UtenteRegistrazioneDto utenteDto) {
+    public ResponseEntity<Object> registrazione(@RequestBody  UtenteRegistrazioneDto utenteDto) {
 
         //controllo validità dto
         //TODO capire se si può fare senza forzare il cast.. inizializzare mappaErrori a Map dà problema con ResponseEntity<Object> in quanto interface
